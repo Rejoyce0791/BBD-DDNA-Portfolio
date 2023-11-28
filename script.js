38a4fc12e3a8b2647efb35f9a858a9a122aa3a10
sthembile0791 <sthembile0791@gmail.com>
2023-11-28 10:01:39 +0200

    weather

--- script.js
+++ script.js
@@ -2,15 +2,36 @@ function downloadCV() {
     // Add code to handle CV download
     alert("Downloading CV...");
 }
+document.addEventListener("DOMContentLoaded", () => {
+    // Replace "YOUR_OPENWEATHERMAP_API_KEY" with your actual OpenWeatherMap API key
+    const apiKey = "873edd09052f07136e63b85accdb7453";
+    
+    // Specify the city name for which you want to get the weather
+    const cityName = "jhb";
 
-function hireMe() {
-    // Add code to handle hiring
-    alert("Hiring process initiated...");
-}
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
 
-// You can add JavaScript functionality here if needed
-document.getElementById('messageForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    // Add logic to handle the form submission (e.g., send an email)
-    alert('Message sent!');
+    // Fetch weather data from OpenWeatherMap API
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            displayWeather(data);
+        })
+        .catch(error => {
+            console.error("Error fetching weather data:", error);
+        });
 });
+
+function displayWeather(data) {
+    const locationElement = document.getElementById("location");
+    const temperatureElement = document.getElementById("temperature");
+    const descriptionElement = document.getElementById("description");
+
+    const location = data.name + ", " + data.sys.country;
+    const temperature = data.main.temp + " °C";
+    const description = data.weather[0].description;
+
+    locationElement.textContent = "Location: " + location;
+    temperatureElement.textContent = "Temperature: " + temperature;
+    descriptionElement.textContent = "Description: " + description;
+}
